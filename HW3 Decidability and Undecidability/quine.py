cfd5f99c846574450a82555c991e2539cb402a1e
treadwell <brooks.ken@gmail.com>
2014-09-23 07:12:35 -0500

    Updates HW 3

---
+++ HW3 Decidability and Undecidability/quine.py
@@ -0,0 +1,14 @@
+x = ['print "x =", x', 'for m in x: print m']
+print "x =", x
+for m in x: print m
+
+print "\n---------\n"
+
+x='x=%s\nprint x%%`x`'
+print x%`x`
+
+
+print "\n---------\n"
+
+x = 'a \nprint x'
+print x
